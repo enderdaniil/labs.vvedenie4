efef09effde6a1693e6ab5779e5011cbaaaecfae
enderdaniil <54796186+enderdaniil@users.noreply.github.com>
2019-09-05 23:00:41 +0300

    Create lab4.4.cpp

---
+++ lab4.4.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    float v1,v2,s,t,s1;
+    cout<<"Vvedite skorosti avtomobilei i skolko kilometrov mezhdu nimi i vremia"<<endl;
+    cin>>v1>>v2>>s>>t;
+    s1=(v1+v2)*t+s;
+    cout<<"Rasstoianie="<<s1<<endl;
+}
