cbd6bf92fb42fae2b737eadb1ccb889b6b1a8d41
enderdaniil <54796186+enderdaniil@users.noreply.github.com>
2019-09-05 22:55:28 +0300

    Update lab4.3.cpp

--- lab4.3.cpp
+++ lab4.3.cpp
@@ -4,8 +4,8 @@ int main()
 {
     float x,p,p1,y,py;
     cout<<"Vvedite X,Y i skolko stoit X konfet i"<<endl;
-    cin>>x>>p;
+    cin>>x>>p>>y;
     p1=x/p;
     py=y*p1;
-    cout<<"1 kilogram stoit"<<p1<<"rub, Y kilogram stoit"<<py<<"rub"<<endl;
+    cout<<"1 kilogram stoit "<<p1<<" rub, Y kilogram stoit "<<py<<" rub"<<endl;
 }
