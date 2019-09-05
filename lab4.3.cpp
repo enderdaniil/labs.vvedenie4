00907724732375dca4bacaab1b1d57e2f032b5a8
enderdaniil <54796186+enderdaniil@users.noreply.github.com>
2019-09-05 22:53:39 +0300

    Create lab4.3.cpp

---
+++ lab4.3.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    float x,p,p1,y,py;
+    cout<<"Vvedite X,Y i skolko stoit X konfet i"<<endl;
+    cin>>x>>p;
+    p1=x/p;
+    py=y*p1;
+    cout<<"1 kilogram stoit"<<p1<<"rub, Y kilogram stoit"<<py<<"rub"<<endl;
+}
