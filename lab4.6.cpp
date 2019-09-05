4209a2ad2d3e994f3c8d4ac0a97dd0d08de85b59
enderdaniil <54796186+enderdaniil@users.noreply.github.com>
2019-09-06 00:16:44 +0300

    Create lab4.6.cpp

---
+++ lab4.6.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    float a1,b1,c1,a2,b2,c2,x,y;
+    cout<<"vvedite koeffizienti"<<endl;
+    cin>>a1>>b1>>c1>>a2>>b2>>c2;
+    x=((c1*b2)-(b1*c2))/((a1*b2)-(a2*b1));
+    y=((a1*c2)-(a2*c1))/((a1*b2)-(a2*b1));
+    cout<<"X="<<x<<", Y="<<y<<endl;
+}
